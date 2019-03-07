8aa0feb58796976818fa946f78cdb32c345cf718
Laura Felce <crepu83@yahoo.it>
2019-03-07 11:08:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
 
     elsif req.path.match(/cart/)
-        @@cart.each do |item|
+        if @@cart.each do |item|
           resp.write "#{item}\n"
       end
       else
@@ -29,6 +29,7 @@ class Application
 
     resp.finish
   end
+end
 
   def handle_search(search_term)
     if @@items.include?(search_term)
