bb4ecb3ad507ed3c6c898e2f1390c06c8d55d7d5
Laura Felce <crepu83@yahoo.it>
2019-03-07 11:35:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,15 @@ class Application
         end
       end
 
+    elsif req.path.match(/add/)
+      add_term = req.params["q"]
+      if @@items.include?(add_term)
+        resp.write "added #{add_term}"
+      else
+        resp.write "We don't have that item"
+      end  
+          
+
     else
       resp.write "Path Not Found"
     end
